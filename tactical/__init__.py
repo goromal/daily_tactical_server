7c73cfd871778ea9b21099080205acdae5442a8e
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-09-07 12:09:12 -0400

    Suppress protobuf version mismatch warnings for now (#6)

---
+++ tactical/__init__.py
@@ -0,0 +1,8 @@
+
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="Protobuf gencode version .* is exactly one major version older.*",
+    module="google.protobuf.runtime_version",
+)
